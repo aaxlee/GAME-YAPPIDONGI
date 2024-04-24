e638db3f9b458c3ee4622bcd21f0080d4c44df32
Axel <axel.adolfsson@skola.taby.se>
2024-04-24 11:06:01 +0200

    better structure

--- script.js
+++ script.js
@@ -142,6 +142,175 @@ function generateCoordinate(max) {
   return Math.floor(Math.random() * max);
 }
 
+function handleMovement() {
+  if (player.direction.right) {
+    player.x += movespeed;
+  }
+  if (player.direction.left) {
+    player.x -= movespeed;
+  }
+  if (player.direction.up) {
+    player.y -= movespeed;
+  }
+  if (player.direction.down) {
+    player.y += movespeed;
+  }
+}
+
+function handleBorder() {
+  if (player.x + size >= WIDTH && player.y + size >= HEIGHT) { // bottom right corner
+    player.x = WIDTH - size;
+    player.y = HEIGHT - size;
+  }
+  if (player.x + size >= WIDTH) { // right
+    player.x = WIDTH - size;
+  }
+  if (player.x <= 0 && player.y <= 0) { // top left corner
+    player.x = 0;
+    player.y = 0;
+  }
+  if (player.y <= 0) { // top
+    player.y = 0;
+  }
+  if (player.x <= 0 && player.y >= HEIGHT - size) { // bottom left
+    player.x = 0;
+    player.y = HEIGHT - size;
+  }
+  if (player.x <= 0) { // left
+    player.x = 0;
+  }
+  if (player.x + size >= WIDTH && player.y <= 0) { // top right
+    player.x = WIDTH - size;
+    player.y = 0;
+  }
+  if (player.y >= HEIGHT - size) { // bottom
+    player.y = HEIGHT - size;
+  }
+}
+
+function handleDash() {
+  if (player.dash.using) {
+    player.immunityFrames.active = true;
+    if (player.dash.iterations < 17) {
+      if (player.direction.right) {
+        player.x += player.dash.distance;
+      }
+      if (player.direction.left) {
+        player.x -= player.dash.distance;
+      }
+      if (player.direction.up) {
+        player.y -= player.dash.distance;
+      }
+      if (player.direction.down) {
+        player.y += player.dash.distance;
+      }
+      player.dash.iterations++;
+    } else {
+      player.dash.using = false;
+      player.dash.iterations = 0;
+      player.immunityFrames.active = false;
+    }
+  }
+}
+
+function handleParry() {
+  if (player.parry.using) {
+    if (player.parry.iterations <= 10) {
+      // check player's direction to determine parry hitbox coordinates
+      if (player.direction.right) {
+        player.parry.hitbox.x = player.x + player.parry.hitbox.size;
+        player.parry.hitbox.y = player.y - (player.height / 2);
+      } else if (player.direction.left) {
+        player.parry.hitbox.x = player.x - player.width - player.parry.hitbox.size;
+        player.parry.hitbox.y = player.y - (player.height / 2);
+      } else if (player.direction.up) {
+        player.parry.hitbox.x = player.x - (player.width / 2);
+        player.parry.hitbox.y = player.y - player.height - player.parry.hitbox.size;
+      } else if (player.direction.down) {
+        player.parry.hitbox.x = player.x - (player.width / 2);
+        player.parry.hitbox.y = player.y + player.parry.hitbox.size;
+      }
+
+      player.parry.iterations++;
+      
+      ctx.beginPath();
+      ctx.fillRect(player.parry.hitbox.x, player.parry.hitbox.y, player.parry.hitbox.size, player.parry.hitbox.size);
+      ctx.stroke();
+    } else {
+      player.parry.using = false;
+      player.parry.iterations = 0;
+    }
+  }
+}
+
+function handleEnemySingleAttack() {
+  if (enemy.projectile.used) {
+    for (let index = 0; index < projectileArray.length; index++) {
+      let projectile = projectileArray[index];
+      if (projectile.x < 0 || projectile.x > WIDTH || projectile.y < 0 || projectile.y > HEIGHT) {
+        projectileArray.splice(index, 1);
+        projectileIndex -= 1;
+      } else {
+        projectile.x += projectile.speed * projectile.direction;
+        ctx.beginPath();
+        ctx.fillRect(projectile.x, projectile.y, 5, 5);
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+function handleEnemyCircleAttack() {
+  if (enemy.attack1.used) {
+    circleAttackArray.forEach(attack => {
+      attack.forEach(projectile => {
+        ctx.beginPath();
+        ctx.fillRect(projectile.x, projectile.y , projectile.size, projectile.size);
+        ctx.stroke();
+        projectile.x += Math.cos(projectile.angle) * projectile.speed;
+        projectile.y += Math.sin(projectile.angle) * projectile.speed;
+      })
+    })
+  }
+}
+
+function handleEnemyTeleportAttack() {
+  if (enemy.teleportAttack.used) {
+    if (enemy.teleportAttack.iterations <= 1) {
+      enemy.x = generateCoordinate(WIDTH);
+      enemy.y = generateCoordinate(HEIGHT);
+      enemy.teleportAttack.target.x = player.x;
+      enemy.teleportAttack.target.y = player.y;
+      enemy.teleportAttack.iterations++;
+    } else if (enemy.teleportAttack.iterations >= 1 && enemy.teleportAttack.iterations <= 30) {
+      let target = {
+        x: enemy.teleportAttack.target.x,
+        y: enemy.teleportAttack.target.y
+      };
+      // if the follow up-attack reaches the target early end the attack
+      if (collision(enemy.x, enemy.y, enemy.size, target.x, target.y, size)) {
+        enemy.teleportAttack.used = false;
+        enemy.teleportAttack.iterations = 0;
+      } else {  // else continue as normal
+        enemy.follow(target, enemy.speed * 10);
+        enemy.teleportAttack.iterations++
+      }
+    } else if (enemy.teleportAttack.iterations >= 30) {
+      enemy.teleportAttack.used = false;
+      enemy.teleportAttack.iterations = 0;
+    }
+  }
+}
+
+function handlePlayerCollision() {
+
+}
+
+function drawObjects() {
+  player.draw();
+  enemy.draw();
+}
+
 // creating player and enemy attacks
 let player = new Player;
 let enemy = new Bot;
@@ -150,7 +319,6 @@ enemy.y = generateCoordinate(HEIGHT - enemy.size);
 
 let projectileIndex = 0;
 let projectileArray = [];
-// let projectile = new Projectile(0, 0, 1, 5, 12, -1);  // -1 angle is placeholder
 
 let circleAttackArray = [];
 let circleAttackIndex = 0;
@@ -247,168 +415,15 @@ function animate(timestamp) {
   lastTimestamp = timestep;
 
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  // change direction
-  if (player.direction.right) {
-    player.x += movespeed;
-  }
-  if (player.direction.left) {
-    player.x -= movespeed;
-  }
-  if (player.direction.up) {
-    player.y -= movespeed;
-  }
-  if (player.direction.down) {
-    player.y += movespeed;
-  }
-  // border
-  if (player.x + size >= WIDTH && player.y + size >= HEIGHT) { // bottom right corner
-    player.x = WIDTH - size;
-    player.y = HEIGHT - size;
-  }
-  if (player.x + size >= WIDTH) { // right
-    player.x = WIDTH - size;
-  }
-  if (player.x <= 0 && player.y <= 0) { // top left corner
-    player.x = 0;
-    player.y = 0;
-  }
-  if (player.y <= 0) { // top
-    player.y = 0;
-  }
-  if (player.x <= 0 && player.y >= HEIGHT - size) { // bottom left
-    player.x = 0;
-    player.y = HEIGHT - size;
-  }
-  if (player.x <= 0) { // left
-    player.x = 0;
-  }
-  if (player.x + size >= WIDTH && player.y <= 0) { // top right
-    player.x = WIDTH - size;
-    player.y = 0;
-  }
-  if (player.y >= HEIGHT - size) { // bottom
-    player.y = HEIGHT - size;
-  }
 
-  // dash
-  if (player.dash.using) {
-    player.immunityFrames.active = true;
-    if (player.dash.iterations < 17) {
-      if (player.direction.right) {
-        player.x += player.dash.distance;
-      }
-      if (player.direction.left) {
-        player.x -= player.dash.distance;
-      }
-      if (player.direction.up) {
-        player.y -= player.dash.distance;
-      }
-      if (player.direction.down) {
-        player.y += player.dash.distance;
-      }
-      player.dash.iterations++;
-    } else {
-      player.dash.using = false;
-      player.dash.iterations = 0;
-      player.immunityFrames.active = false;
-    }
-  }
-
-  // parry
-  if (player.parry.using) {
-    if (player.parry.iterations <= 10) {
-      // check player's direction to determine parry hitbox coordinates
-      if (player.direction.right) {
-        player.parry.hitbox.x = player.x + player.parry.hitbox.size;
-        player.parry.hitbox.y = player.y - (player.height / 2);
-      } else if (player.direction.left) {
-        player.parry.hitbox.x = player.x - player.width - player.parry.hitbox.size;
-        player.parry.hitbox.y = player.y - (player.height / 2);
-      } else if (player.direction.up) {
-        player.parry.hitbox.x = player.x - (player.width / 2);
-        player.parry.hitbox.y = player.y - player.height - player.parry.hitbox.size;
-      } else if (player.direction.down) {
-        player.parry.hitbox.x = player.x - (player.width / 2);
-        player.parry.hitbox.y = player.y + player.parry.hitbox.size;
-      }
-
-      player.parry.iterations++;
-      
-      ctx.beginPath();
-      ctx.fillRect(player.parry.hitbox.x, player.parry.hitbox.y, player.parry.hitbox.size, player.parry.hitbox.size);
-      ctx.stroke();
-    } else {
-      player.parry.using = false;
-      player.parry.iterations = 0;
-    }
-  }
-
-  // attack
-  if (enemy.projectile.used) {
-    // projectileArray.forEach(projectile => {
-    //   projectile.x += projectile.speed * projectile.direction;
-
-    //   if (projectile.x < 0 || projectile.x > WIDTH || projectile.y < 0 || projectile.y > HEIGHT) {
-    //     unset(projectile);
-    //     projectileIndex -= 1;
-    //   } else {
-    //     ctx.beginPath();
-    //     ctx.fillRect(projectile.x, projectile.y, 5, 5);
-    //     ctx.stroke();
-    //   }
-    // })
-    for (let index = 0; index < projectileArray.length; index++) {
-      let projectile = projectileArray[index];
-      if (projectile.x < 0 || projectile.x > WIDTH || projectile.y < 0 || projectile.y > HEIGHT) {
-        projectileArray.splice(index, 1);
-        projectileIndex -= 1;
-      } else {
-        projectile.x += projectile.speed * projectile.direction;
-        ctx.beginPath();
-        ctx.fillRect(projectile.x, projectile.y, 5, 5);
-        ctx.stroke();
-      }
-    }
-  }
-
-  if (enemy.attack1.used) {
-    circleAttackArray.forEach(attack => {
-      attack.forEach(projectile => {
-        ctx.beginPath();
-        ctx.fillRect(projectile.x, projectile.y , projectile.size, projectile.size);
-        ctx.stroke();
-        projectile.x += Math.cos(projectile.angle) * projectile.speed;
-        projectile.y += Math.sin(projectile.angle) * projectile.speed;
-      })
-    })
-  }
-
-  if (enemy.teleportAttack.used) {
-    if (enemy.teleportAttack.iterations <= 1) {
-      enemy.x = generateCoordinate(WIDTH);
-      enemy.y = generateCoordinate(HEIGHT);
-      enemy.teleportAttack.target.x = player.x;
-      enemy.teleportAttack.target.y = player.y;
-      enemy.teleportAttack.iterations++;
-    } else if (enemy.teleportAttack.iterations >= 1 && enemy.teleportAttack.iterations <= 30) {
-      let target = {
-        x: enemy.teleportAttack.target.x,
-        y: enemy.teleportAttack.target.y
-      };
-      // if the follow up-attack reaches the target early end the attack
-      if (collision(enemy.x, enemy.y, enemy.size, target.x, target.y, size)) {
-        enemy.teleportAttack.used = false;
-        enemy.teleportAttack.iterations = 0;
-      } else {  // else continue as normal
-        enemy.follow(target, enemy.speed * 10);
-        enemy.teleportAttack.iterations++
-      }
-    } else if (enemy.teleportAttack.iterations >= 30) {
-      enemy.teleportAttack.used = false;
-      enemy.teleportAttack.iterations = 0;
-    }
-  }
-
+  handleMovement();
+  handleBorder();
+  handleDash();
+  handleParry();
+  handleEnemySingleAttack();
+  handleEnemyCircleAttack();
+  handleEnemyTeleportAttack();
+  handlePlayerCollision();
 
   if (!enemy.teleportAttack.used) {
     enemy.follow(player, enemy.speed);
@@ -416,12 +431,8 @@ function animate(timestamp) {
 
   // check if player got hit
 
+  drawObjects();
 
-  // draw player
-  player.draw();
-  
-  // draw enemy
-  enemy.draw();
   requestAnimationFrame(animate);
 }
 
